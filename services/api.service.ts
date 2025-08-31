ad8de3a0db476c2406077eb241df9cc997de14d7
oozaw <79121990+oozaw@users.noreply.github.com>
2025-09-01 00:15:46 +0700

    update: axios configuration

--- services/api.service.ts
+++ services/api.service.ts
@@ -2,8 +2,9 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: process.env.BASE_API_URL || "http://localhost:5000/api",
+  baseURL: process.env.NEXT_PUBLIC_BASE_API_URL || "http://localhost:5000/api",
 });
+
 
 api.interceptors.request.use(
   (config) => {
